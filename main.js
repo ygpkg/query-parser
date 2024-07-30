d8c5350b18189cb26df90cbddb7522802839e851
Chuanjian Wang <chuanjwang@tencent.com>
2024-07-30 11:48:54 +0800

    fix parser

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ const parser = require("./query-language-parser");
 
 testdata = {
     "Key1:Value1": '{"key":"Key1","value":["Value1"]}',
+    "Key1:NOT Value1": '{"op":"NOT","key":"Key1","value":["Value1"]}',
     "Key1:'Val ue1'": '{"key":"Key1","value":["s你好啊"]}',
     'Key1:"Val ue1"': '{"key":"Key1","value":["Val ue1"]}',
-    "Key1:(s你好啊)": '{"key":"Key1","value":["s你好啊"]}',
+    'Key1:NOT "Val ue1"': '{"op":"NOT","key":"Key1","value":["Val ue1"]}',
+    "Key1:(Value1)": '{"op":"OR","exprs":[{{"key":"Key1","value":["Value1"]}]}',
+    "Key1:(Value1 OR Value2)": '{"op":"OR","exprs":[{{"key":"Key1","value":["Value1"]}, {"key":"Key1","value":["Value2"]}]}',
+    "Key1:(Value1 AND Value2)": '{"op":"AND","exprs":[{{"key":"Key1","value":["Value1"]}, {"key":"Key1","value":["Value2"]}]}',
+    "Key1:(Value1 AND NOT Value2)": '{"op":"AND","exprs":[{{"key":"Key1","value":["Value1"]}, {"op":"NOT","exprs":[{"key":"Key1","value":["Value2"]}]}]}',
+    "Key1:(Value1 NOT Value2)": '{"op":"AND","exprs":[{"key":"Key1","value":["Value1"]}, {"op":"NOT","key":"Key1","value":["Value2"]}]}',
     "NOT Key1:Value1": '{"op":"NOT","exprs":[{"key":"Key1","value":["Value1"]}]}',
     "Key1:Value1 AND Key2:Value2": '{"op":"AND","exprs":[{"key":"Key1","value":["Value1"]},{"key":"Key2","value":["Value2"]}]}',
+    "Key1:(Value1 OR Value2)": '{"op":"OR","exprs":[{"key":"Key1","value":["Value1"]}, {"key":"Key1","value":["Value2"]}]}',
+    "Key1:(Value1 AND Value2)": '{"op":"AND","exprs":[{"key":"Key1","value":["Value1"]}, {"key":"Key1","value":["Value2"]}]}',
     "Key1:Value1 AND (Key2:Value2 OR Key3:Value3)": '{"op":"AND","exprs":[{"key":"Key1","value":["Value1"]},{"op":"OR","exprs":[{"key":"Key2","value":["Value2"]},{"key":"Key3","value":["Value3"]}]}]}',
     "Key1:Value1 AND Key2:(Value2 OR Value3)": '{"op":"AND","exprs":[{"key":"Key1","value":["Value1"]},{"op":"OR","exprs":[{"key":"Key2","value":["Value2"]},{"key":"Key2","value":["Value3"]}]}]}',
+    "DESC:(手术 NOT (疾病 OR 发明))": '{"op":"AND","exprs":[{"key":"DESC","value":["手术"]},{"op":"OR","exprs":[{"op":"NOT","key":"DESC","value":["疾病"]},{"op":"NOT","key":"DESC","value":["发明"]}]}]}',
+    "ANCS:(功) and TA:(智能 OR 医学) AND DESC:(手术 NOT (疾病 OR 发明))": '{"op":"AND","exprs":[{"key":"ANCS","value":["功"]},{"op":"OR","exprs":[{"key":"TA","value":["智能"]},{"key":"TA","value":["医学"]}]},{"op":"AND","exprs":[{"key":"DESC","value":["手术"]},{"op":"OR","exprs":[{"op":"NOT","key":"DESC","value":["疾病"]},{"op":"NOT","key":"DESC","value":["发明"]}]}]}]}',
 }
 // const queryStr = "Key1:Value1 AND (Key2:(Value2 OR Value3)) OR Key3:(NOT Value4)";
 const queryStr = "Key1:Value1";
